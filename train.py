fb3748c3336037cf6c28894932b039c4ce67c4b6
MacBook Pro <macbookpro@MacBook-Pro-cua-MacBook.local>
2020-03-23 21:10:21 +0700

    ok:

--- train.py
+++ train.py
@@ -15,7 +15,8 @@ from keras.optimizers import Adam, RMSprop
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from sklearn.utils import class_weight
-from model import tiny_XCEPTION, little_XCEPTION
+from model import tiny_XCEPTION, little_XCEPTION, simple_CNN
+from mobilenet import MobileNet
 # from my_generator2 import My_Generator
 
 # net = applications.mobilenet_v2.MobileNetV2(include_top=False, pooling='avg', weights='imagenet',
@@ -30,29 +31,30 @@ from model import tiny_XCEPTION, little_XCEPTION
 
 # for layer in net.layers[:-45]:
 #    layer.trainable = False
-model = little_XCEPTION((112, 112, 3), 2)
+model = MobileNet((64, 64, 3), 200)
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
+# exit(0)
 print("Compile model done!")
 
 
 earlyStopping = EarlyStopping(monitor='val_acc', patience=20, verbose=1)
-filepath = "models/gb_model.h5"
+filepath = "models/imagenet_clf_model_test.h5"
 mcp_save = ModelCheckpoint(filepath, save_best_only=True, monitor='val_acc')
 reduce_lr = ReduceLROnPlateau('val_acc', factor=0.5,
                                 patience=4, verbose=1)
 
 train_data_dir = 'data/train'
-validation_data_dir = 'data/test'
-batch_size = 32
+validation_data_dir = 'data/validation'
+batch_size = 128
 
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
-    zoom_range=0.1,
+    # zoom_range=0.1,
     #brightness_range=(0.7, 1.25),
-    #width_shift_range=0.2,
-    #height_shift_range=0.2,
+    # width_shift_range=0.15,
+    # height_shift_range=0.15,
     shear_range=0.11,
     rotation_range=15,
     horizontal_flip=True)
@@ -61,13 +63,13 @@ validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
-    target_size=(112, 112),
+    target_size=(64, 64),
     batch_size=batch_size,
     class_mode='categorical')
 
 validation_generator = validation_datagen.flow_from_directory(
     validation_data_dir,
-    target_size=(112, 112),
+    target_size=(64, 64),
     batch_size=batch_size,
     class_mode='categorical',
     shuffle = False)
@@ -79,5 +81,10 @@ class_weights = class_weight.compute_class_weight(
 print(class_weights)
 print(train_generator.class_indices)
 
-model.fit_generator(generator=train_generator, epochs=50, verbose=1, class_weight=class_weights, \
-        callbacks=[earlyStopping, mcp_save, reduce_lr], validation_data=validation_generator)
+step_per_epoch = len(glob.glob(os.path.join(train_data_dir, '*/*'))) // batch_size
+validation_steps = len(glob.glob(os.path.join(validation_data_dir, '*/*'))) // batch_size
+print(step_per_epoch)
+print(validation_steps)
+
+model.fit_generator(generator=train_generator, epochs=50, verbose=1, class_weight=class_weights, steps_per_epoch=step_per_epoch,\
+        callbacks=[earlyStopping, mcp_save, reduce_lr], validation_data=validation_generator, validation_steps=validation_steps)
